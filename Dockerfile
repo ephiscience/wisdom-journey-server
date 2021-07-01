c6f18102f4dd73a3daa8760b46e9f04ccefffd8f
Rémi Piotaix <piotaixr@users.noreply.github.com>
2021-07-02 01:27:14 +0200

    Fix deployment config (#9)
    - Add logger
    - Add graceful shutdown

--- Dockerfile
+++ Dockerfile
@@ -8,13 +8,20 @@ COPY . .
 RUN yarn prisma generate && yarn build
 # Ici, l'application est compilée
 
+FROM node:14-alpine AS deps
+WORKDIR /app
+
+COPY package.json yarn.lock ./
+COPY prisma .
+RUN yarn install --production && yarn prisma generate
+
 FROM node:14-alpine
 EXPOSE 4000
 WORKDIR /app
 
 COPY package.json yarn.lock src/context.ts docker/entrypoint.sh ./
 COPY prisma .
-RUN yarn install --production && yarn prisma generate
+COPY --from=deps /app/node_modules ./node_modules
 
 COPY --from=build /app/dist/src ./
 CMD [ "/bin/sh", "/app/entrypoint.sh" ]
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import { server } from "./server";
+import express from "express";
+import morgan from "morgan";
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+const app = express();
+
+app.use(morgan("combined"));
+server.applyMiddleware({ app, path: "/api" });
+
+new Promise<void>((resolve) => {
+  const server = app.listen({ port: 4000 }, () => resolve());
+
+  process.on("SIGTERM", () => {
+    server.close(() => {
+      console.log("HTTP server closed");
+    });
+  });
+}).then((server) => {
+  console.log(`🚀 Server ready`);
 });
